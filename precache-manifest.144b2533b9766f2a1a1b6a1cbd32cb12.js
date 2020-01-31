22c261adda8965254d3317878131dffb86903803
soun059 <soujanna2015newera@gmail.com>
2020-01-31 14:45:18 +0530

    Updates

---
+++ precache-manifest.144b2533b9766f2a1a1b6a1cbd32cb12.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d89115592a568e42d3f4c0405b1d51ef",
+    "url": "/Teacher_recommend/index.html"
+  },
+  {
+    "revision": "00b8f2ec32737d438cb1",
+    "url": "/Teacher_recommend/static/css/main.b7c23dd9.chunk.css"
+  },
+  {
+    "revision": "23e39b5302053a3eb97d",
+    "url": "/Teacher_recommend/static/js/2.98714669.chunk.js"
+  },
+  {
+    "revision": "e2fc3609d47d90b8cc004bfe15ddf15e",
+    "url": "/Teacher_recommend/static/js/2.98714669.chunk.js.LICENSE"
+  },
+  {
+    "revision": "00b8f2ec32737d438cb1",
+    "url": "/Teacher_recommend/static/js/main.a76a4716.chunk.js"
+  },
+  {
+    "revision": "a9e9a5fa3bd9ad5bde95",
+    "url": "/Teacher_recommend/static/js/runtime-main.9c50904f.js"
+  }
+]);
